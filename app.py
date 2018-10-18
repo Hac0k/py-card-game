09e49244c4816c9570544c43592880b7ba968573
wovalles <wovalles@itenllisys.com.do>
2018-10-18 12:46:50 -0400

    wokr

--- app.py
+++ app.py
@@ -1,22 +1,7 @@
 from Tkinter import *
-<<<<<<< HEAD
-from gui.main import main
+from gui.start import Application
 
-class Application():
 
-      def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        # main1 = main()
-        # createwidgest = main1.createWidgets(self)
-        createwidgest = main.createWidgets()
-
-        self.createwidgest
-
-=======
-import gui.start  
-    
->>>>>>> a6cba43112de4309e52e2404892962a4b96884fa
 root = Tk()
 app = gui.start.Application(master=root)
 app.mainloop()

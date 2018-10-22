312694700ab786eacd4eebc02d8c193e5674eba3
wovalles <wovalles@itenllisys.com.do>
2018-10-22 13:39:30 -0400

    add cards

---
+++ funcionality/cards.py
@@ -0,0 +1,12 @@
+class Cards():
+    heart=chr(3)
+    diamons =chr(4)
+    club = chr(5)
+    spade =chr(6)
+    def cards():
+        return cards=[[heart,diamons,club,spade],['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q ', 'K']]
+    
+
+
+    def __init__(self, *args):
+        

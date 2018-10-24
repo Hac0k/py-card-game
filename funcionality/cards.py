6061e811a397fa006b94e46060fd12cc79e97928
Hac0k <wilsonovalles96@gmail.com>
2018-10-23 22:09:34 -0400

    nada

--- funcionality/cards.py
+++ funcionality/cards.py
@@ -1,11 +1,15 @@
-class Cards():
+import random
+class Card():
     heart=chr(3)
     diamons =chr(4)
     club = chr(5)
     spade =chr(6)
+    a = 1
     def cards():
-        return cards=[[heart,diamons,club,spade],['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q ', 'K']]
-    
+    	cards=[a, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q ', 'k']
+    	{heart:random.shuffle(cards)}
+    	
+    	return 
 
 
     def __init__(self, *args):
---
+++ funcionality/deckOfCards.py
@@ -0,0 +1,16 @@
+from card import *
+class deck(object):
+	
+	"""docstring for deck"""
+	def deck():
+		for i in range(4): 
+			card.cards
+		
+	def firstDeck(cards):
+
+		
+
+	def __init__(self, arg):
+		super(deck, self).__init__()
+		self.arg = arg
+		

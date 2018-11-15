061a18f995470c9416197a5a776c21e8dd3e7089
wovalles <wovalles@intellisys.com.do>
2018-11-14 23:19:28 -0400

    logic to players

--- Game.py
+++ Game.py
@@ -3,9 +3,11 @@ from funcionality.player import *
 
 from gui import *
 
-class setting(object):
+class Setting(object):
 	"""docstring for settingg"""
-	global setting,players
+	global setting,players,msg
+	msg={'numberIncorrect':"The number is more big than 4 or 0 choice AUTOSELECT 4, do you reselection of players? Y or N",
+	'howplayer':"how many player 2,3,4",'autoselect':'-AUTOSELECT 4','troll':"u cant troll me give a number"}
 	setting={}
 	players=[]
 
@@ -13,34 +15,49 @@ class setting(object):
 		print "Your name "
 		user = raw_input()
 		players.append(player(user).players())
-		print "how many player 2,3,4"
-		numberPlayers = raw_input()
-		return self.createAiPlayers(numberPlayers)	
+		return self.createAiPlayers()	
 
-	def createAiPlayers(self,numberPlayers):
-		
-		if int(numberPlayers) >=5:
-			print "The number is more big than 4 choice AUTOSELECT 4, do you reselection of players? Y or N"
-			entry=raw_input()
-			if (entry == "yes" or entry == "y"):
-				print "how many player 2,3,4"
-				number=raw_input()
-				int(number)-1
-				numberPlayers = number if number > 4 else 4
-				print numberPlayers
-				return self.createAiPlayers(numberPlayers)	
-		for i in range(int(numberPlayers)-1):
-			players.append(player(None).players())
-		return players
+	def createAiPlayers(self,numberPlayers=None):
+		try:
+			print msg['howplayer']
+			numberPlayers=raw_input()
+			int(numberPlayers)
+			if int(numberPlayers) >=5 or int(numberPlayers) == 0:
+				print msg['numberIncorrect']
+				entry=raw_input()
+				if (entry == "yes" or entry == "y"):
+					print msg['howplayer']
+					try:
+						number=raw_input()
+						int(number-1)
+						numberPlayers = number if number > 4 or number != 0 else 4
+						return self.createAiPlayers(numberPlayers)
+					except ValueError:
+						print msg['troll'],msg['autoselect']
+						return self.createAiPlayers(numberPlayers=4)
+				else:
+					return self.createAiPlayers(numberPlayers=4)
+			for i in range(int(numberPlayers)-1):
+				players.append(player(None).players())
+			setting['players'] = players
+		except ValueError:
+			print msg['troll']
+			return self.createAiPlayers(numberPlayers=4)
 
-class game(object):
-	"""docstring for table"""
-	"""{'name':playerName,'status':'ai','cards':[],"statusShuffle":False,}	"""
-	players = setting().createPlayers()
-	"""templateDict ={'number':number,'simbol':simbol,'status':'maindeck','value':(cards[1].index(number))+1,'img':None}"""
-	deck = deck().shuffle()
 	
-	table=[]
+
+ 	def settings(self):
+ 		self.createPlayers()
+		return setting
+
+class game():
+	players = Setting().settings()
+	deck = deck().shuffle()
+
+	
+
+	# print deck
+	
 
 
 
